69b47f018e148d1045c1663cbede63746ef776f3
vladislav.bukharov <vladislav.bukharov@presight.ai>
2025-12-25 17:03:02 +0400

    added pmd

--- src/test/java/org/household/inventory/HouseholdInventoryApplicationTests.java
+++ src/test/java/org/household/inventory/HouseholdInventoryApplicationTests.java
@@ -1,6 +1,13 @@
 package org.household.inventory;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -10,6 +17,7 @@ class HouseholdInventoryApplicationTests {
 
   @Test
   void contextLoads() {
+    assertDoesNotThrow(() -> true);
   }
 
 }
