efbf110326ef5b3885c43227e8fa45410a9d2e6c
vladislav.bukharov <vladislav.bukharov@presight.ai>
2026-01-07 16:48:23 +0400

    added another endpoint to test deployment removed check and test tasks from building process in Docker

--- src/main/java/org/household/inventory/api/HelloWorldController.java
+++ src/main/java/org/household/inventory/api/HelloWorldController.java
@@ -2,6 +2,7 @@ package org.household.inventory.api;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Test controller. */
@@ -16,5 +17,10 @@ public class HelloWorldController {
   @GetMapping("/hello")
   public ResponseEntity<String> helloWorld() {
     return ResponseEntity.ok("Hello, World!");
+  }
+
+  @GetMapping("/hello/{name}")
+  public ResponseEntity<String> hello(@PathVariable("name") String name) {
+    return ResponseEntity.ok("Hello, " + name + "!");
   }
 }
