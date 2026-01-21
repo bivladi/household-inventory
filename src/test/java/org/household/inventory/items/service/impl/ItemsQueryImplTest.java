50bf3645b48f6c648dc16ccbd551202456b7e8f4
vladislav.bukharov <vladislav.bukharov@presight.ai>
2026-01-21 15:36:50 +0400

    added test

--- src/test/java/org/household/inventory/items/service/impl/ItemsQueryImplTest.java
+++ src/test/java/org/household/inventory/items/service/impl/ItemsQueryImplTest.java
@@ -1,17 +1,16 @@
 package org.household.inventory.items.service.impl;
 
-import org.household.inventory.items.repository.ItemsRepository;
-import org.household.inventory.model.Item;
-import org.junit.jupiter.api.Test;
-
-import java.util.Optional;
-import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.household.inventory.items.repository.ItemsRepository;
+import org.household.inventory.model.Item;
+import org.junit.jupiter.api.Test;
 
 class ItemsQueryImplTest {
 
@@ -28,10 +27,10 @@ class ItemsQueryImplTest {
 
     final var value = new Item();
     final var id = UUID.randomUUID();
-                value.setId(id);
-        when(repositoryMock.findById(eq(id))).thenReturn(Optional.of(value));
+    value.setId(id);
+    when(repositoryMock.findById(eq(id))).thenReturn(Optional.of(value));
 
-                final var item = subject.findById(id);
+    final var item = subject.findById(id);
 
     assertTrue(item.isPresent());
     assertEquals(id, item.get().getId());
